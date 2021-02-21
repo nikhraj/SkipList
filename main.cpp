ddeaeef007f146112132d92c6b9d1fb699fb15d2
Nikhil Rajput <nikhil2018.rajput@gmail.com>
2021-02-21 17:48:04 +0000

    added display and delete

--- main.cpp
+++ main.cpp
@@ -44,14 +44,14 @@ void insert(int val)
 
 }
 
-bool find(int val)
+Node* find(int val)
 {
   auto p=head;
   int level=2;
   while(p->l[level]||level)
   {
     if(level==-1)
-      return false;
+      return NULL;
     if(!p->l[level])
     {
       level--;
@@ -61,13 +61,50 @@ bool find(int val)
       p=p->l[level];
     }
     else if(p->l[level]->data==val)
-      return true;
+      return p->l[level];
     else
     {
       level--;
     }
   }
-  return false;
+  return NULL;
+}
+
+void delete_val(int val)
+{
+    Node *deleteNode = find(val);
+    if(!deleteNode)
+      return;
+    auto it = find(m.begin(),m.end(),deleteNode);
+    int index = it-m.begin();
+    m[index-1]->l[0]=m[index]->l[0];
+    m.erase(it);
+    for(int i=0;i<index;i++)
+    {
+      for(int j=1;j<3;j++)
+      {
+        m[i]->l[j] = m[i+(1<<j)+1];
+      }
+    }
+    cout<<"Element "<<val<<"is deleted."<<endl;
+    s--;
+}
+
+void display()
+{
+  int level=0;
+  while(level<=2)
+  {
+    auto p=head;
+    cout<<"Level "<<level<<" : ";
+    while(p)
+    {
+      cout<<p->data<<" ";
+      p=p->l[level];
+    }
+    level++;
+  }
+
 }
 
 int main() {
@@ -80,13 +117,79 @@ int main() {
   insert(4);
   insert(6);
   insert(7);
-  /*for(int i=0;i<s;i++)
+  ifstream input;
+	input.open("data.txt");
+  int val;
+  int choice;
+  int file=0;
+  do
   {
-      cout<<m[i]->data<<" ";
-  }*/
-  auto  p =head;
-  cout<<head->l[1]->data<<" ";
-  cout<<find(8)<<endl; 
+      if(file)
+      {
+        input>>choice;
+      }
+      else
+      {
+        cout<<"\nSkipList Operations\n";
+        cout<<"1. Insert"<<endl;
+        cout<<"2. Search"<<endl;
+        cout<<"3. Delete"<<endl;
+        cout<<"4. Display"<<endl;
+        cout<<"5. End"<<endl;
+        cout<<"Enter your Choice: ";
+        cin>>choice;
+      }
+      switch (choice)
+      {
+      case 1 :
+        if(file)
+        {
+          input>>val;
+        }
+        else{
+          cout<<"Enter integer element to insert: ";
+          cin>>val;
+        }
+        insert(val);
+        break;
+      case 2 :
+        if(file)
+        {
+          input>>val;
+        }
+        else
+        {
+          cout<<"Enter integer element to search: ";
+          cin>>val;
+        }
+        if (find(val)!=NULL)
+            cout<<val<<"Element is found in the skip list"<<endl;
+        else
+            cout<<val<<"Element is not found"<<endl;
+        break;
+      case 3:
+        if(file)
+        {
+          input>>val;
+        }
+        else{
+          cout<<"Enter integer element to delete: ";
+          cin>>val;
+        }
+        delete_val(val);
+        break;
+      case 4 :
+          display();
+          break;
+      case 5:
+          break;
+      }
+  } while (choice!=5);
+  input.close();
+
+  //auto  p =head;
+  //cout<<head->l[1]->data<<" ";
+  //cout<<find(8)<<endl; 
   /*while(p)
   {
     p=p->l[0];
